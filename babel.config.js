ae2b6b657b1e153951ace4c2929d7718c2b50885
Lucas Munhoz <ln.munhoz@gmail.com>
2019-12-23 12:50:09 +0700

    Upgrade deps

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: ["next/babel"],
+  plugins: [
+    [
+      "emotion",
+      {
+        // sourceMap is on by default but source maps are dead code eliminated in production
+        sourceMap: true,
+        autoLabel: process.env.NODE_ENV !== "production",
+        labelFormat: "[local]",
+        cssPropOptimization: true
+      }
+    ]
+  ]
+};
