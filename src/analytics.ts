6729a0ea57d457b9853cbd68b5f1e4a068c7c0ab
Lucas Munhoz <ln.munhoz@gmail.com>
2019-12-23 13:26:47 +0700

    Only enable GA in production

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,9 +1,9 @@
 import ReactGA from "react-ga";
 
 export function trackPageView() {
-  // if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-  //   return;
-  // }
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    return;
+  }
   // @ts-ignore
   if (!window.GA_INITIALIZED) {
     ReactGA.initialize("UA-154925075-1");
